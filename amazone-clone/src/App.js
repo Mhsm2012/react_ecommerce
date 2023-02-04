aaac4159f7ce296e8b33dc05483fcf243731d46e
mohamed <eliwa554@gmail.com>
2023-02-04 13:27:43 +0300

    product

--- amazone-clone/src/App.js
+++ amazone-clone/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './Header';
 import Login from './Login';
 import Home from './Home';
+
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 
@@ -22,6 +23,7 @@ function App() {
           <Header/>
           <Home/>
         </Route>
+        
        
       </Switch>
     </div>
--- amazone-clone/src/Home.js
+++ amazone-clone/src/Home.js
@@ -1,9 +1,29 @@
 import React from "react";
+import Product from "./Product";
+import './Home.css'
 
 
 function Home(){
     return(
-        <h1>Home</h1>
+        <div className="home">
+            <img className="home__image" src="https://images-na.ssl-images-amazon.com/images/G/01/img18/home/2022/Q3/Onsite/StoreFronts/2022Q3_HCT_Storefront_Hero_NewTraditional_D_1500x500_EN.jpg" alt="Create a lived-in, modern look Shop now"></img>
+        
+        <div className="home__row">
+        <Product  id ="12321" title="The lean startup" price = {11.87} rating ={4} image="https://m.media-amazon.com/images/G/40/gno/sprites/nav-sprite-global-1x-hm-dsk-reorg._CB405931308_.png" />
+            
+        </div>
+        <div className="home__row">
+        <Product  id ="12321" title="The lean startup" price = {11.87} rating ={4} image="https://m.media-amazon.com/images/G/40/gno/sprites/nav-sprite-global-1x-hm-dsk-reorg._CB405931308_.png" />
+            
+        </div>
+        <div className="home__row">
+        <Product  id ="12321" title="The lean startup" price = {11.87} rating ={4} image="https://m.media-amazon.com/images/G/40/gno/sprites/nav-sprite-global-1x-hm-dsk-reorg._CB405931308_.png" />
+            
+        </div>
+
+
+
+        </div>
     )
 }
 export default Home
